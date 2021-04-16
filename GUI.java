3578f7b8fc058cbdd88878efe4c6f69cd0655c09
Vandja05 <82669558+Vandja05@users.noreply.github.com>
2021-04-16 11:11:46 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* first implimentation of the Debugger gui, its design used the buttons lab, we did previous in class, as a base.
  */
 package gui;
 
